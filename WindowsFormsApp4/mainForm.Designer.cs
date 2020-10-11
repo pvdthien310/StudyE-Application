d4528464ce3e432262be2a8a4d632d66de85969c
Minhthang1506 <19522216@gm.uit.edu.vn>
2020-10-11 14:44:52 +0700

    Update mainForm.Designer.cs

--- WindowsFormsApp4/mainForm.Designer.cs
+++ WindowsFormsApp4/mainForm.Designer.cs
@@ -31,18 +31,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
             this.VietAnhTab = new System.Windows.Forms.TabControl();
             this.AnhVietTab = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.TraDoanTab = new System.Windows.Forms.TabPage();
             this.TuDaTra = new System.Windows.Forms.TabPage();
             this.TuBatQuyTacTab = new System.Windows.Forms.TabPage();
-            this.ThemTuMoiTab = new System.Windows.Forms.TabPage();
-<<<<<<< HEAD
-            this.button2 = new System.Windows.Forms.Button();
-=======
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
->>>>>>> master
+            this.ThemTuMoiTab = new System.Windows.Forms.TabPage();
             this.VietAnhTab.SuspendLayout();
             this.AnhVietTab.SuspendLayout();
             this.TuBatQuyTacTab.SuspendLayout();
@@ -68,10 +65,7 @@
             // 
             // AnhVietTab
             // 
-<<<<<<< HEAD
             this.AnhVietTab.Controls.Add(this.button2);
-=======
->>>>>>> master
             this.AnhVietTab.Controls.Add(this.button1);
             this.AnhVietTab.Controls.Add(this.comboBox1);
             this.AnhVietTab.Location = new System.Drawing.Point(4, 22);
@@ -82,18 +76,21 @@
             this.AnhVietTab.Text = "Anh -  Việt";
             this.AnhVietTab.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(502, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // button1
             // 
-<<<<<<< HEAD
-            this.button1.Location = new System.Drawing.Point(517, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(36, 23);
-=======
-            this.button1.Location = new System.Drawing.Point(776, 83);
+            this.button1.Location = new System.Drawing.Point(523, 84);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(54, 35);
->>>>>>> master
+            this.button1.Size = new System.Drawing.Size(54, 22);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -101,12 +98,8 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-<<<<<<< HEAD
-            this.comboBox1.Location = new System.Drawing.Point(216, 55);
-=======
-            this.comboBox1.Location = new System.Drawing.Point(324, 85);
+            this.comboBox1.Location = new System.Drawing.Point(210, 85);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
->>>>>>> master
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(295, 21);
             this.comboBox1.TabIndex = 0;
@@ -146,22 +139,24 @@
             // 
             this.TuBatQuyTacTab.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.hinh_nen_dep_cho_laptop_1366x768_1;
             this.TuBatQuyTacTab.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-<<<<<<< HEAD
-            this.TuBatQuyTacTab.Location = new System.Drawing.Point(4, 22);
-            this.TuBatQuyTacTab.Name = "TuBatQuyTacTab";
-            this.TuBatQuyTacTab.Padding = new System.Windows.Forms.Padding(3);
-            this.TuBatQuyTacTab.Size = new System.Drawing.Size(788, 424);
-=======
             this.TuBatQuyTacTab.Controls.Add(this.dataGridView1);
             this.TuBatQuyTacTab.Location = new System.Drawing.Point(4, 22);
             this.TuBatQuyTacTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TuBatQuyTacTab.Name = "TuBatQuyTacTab";
             this.TuBatQuyTacTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.TuBatQuyTacTab.Size = new System.Drawing.Size(1186, 666);
->>>>>>> master
+            this.TuBatQuyTacTab.Size = new System.Drawing.Size(788, 424);
             this.TuBatQuyTacTab.TabIndex = 4;
             this.TuBatQuyTacTab.Text = "Từ bất quy tắc";
             this.TuBatQuyTacTab.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(408, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
             // 
             // ThemTuMoiTab
             // 
@@ -175,26 +170,6 @@
             this.ThemTuMoiTab.TabIndex = 5;
             this.ThemTuMoiTab.Text = "Thêm từ mới";
             this.ThemTuMoiTab.UseVisualStyleBackColor = true;
-            // 
-<<<<<<< HEAD
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(478, 201);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-=======
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(408, 89);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 0;
->>>>>>> master
             // 
             // mainForm
             // 
@@ -225,10 +200,7 @@
         private System.Windows.Forms.TabPage TuBatQuyTacTab;
         private System.Windows.Forms.TabPage ThemTuMoiTab;
         private System.Windows.Forms.Button button1;
-<<<<<<< HEAD
         private System.Windows.Forms.Button button2;
-=======
         private System.Windows.Forms.DataGridView dataGridView1;
->>>>>>> master
     }
 }
