7065533b8475a5331428869b20e31fa90868f25a
Minhthang1506 <19522216@gm.uit.edu.vn>
2020-10-11 14:28:37 +0700

    u

--- WindowsFormsApp4/mainForm.Designer.cs
+++ WindowsFormsApp4/mainForm.Designer.cs
@@ -35,8 +35,9 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.TraDoanTab = new System.Windows.Forms.TabPage();
             this.TuDaTra = new System.Windows.Forms.TabPage();
-            this.ThemTuMoiTab = new System.Windows.Forms.TabPage();
             this.TuBatQuyTacTab = new System.Windows.Forms.TabPage();
+            this.ThemTuMoiTab = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.VietAnhTab.SuspendLayout();
             this.AnhVietTab.SuspendLayout();
             this.SuspendLayout();
@@ -51,22 +52,21 @@
             this.VietAnhTab.Controls.Add(this.TuBatQuyTacTab);
             this.VietAnhTab.Controls.Add(this.ThemTuMoiTab);
             this.VietAnhTab.ItemSize = new System.Drawing.Size(132, 18);
-            this.VietAnhTab.Location = new System.Drawing.Point(2, 2);
-            this.VietAnhTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.VietAnhTab.Location = new System.Drawing.Point(1, 1);
             this.VietAnhTab.Name = "VietAnhTab";
             this.VietAnhTab.SelectedIndex = 0;
-            this.VietAnhTab.Size = new System.Drawing.Size(1194, 692);
+            this.VietAnhTab.Size = new System.Drawing.Size(796, 450);
             this.VietAnhTab.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.VietAnhTab.TabIndex = 0;
             // 
             // AnhVietTab
             // 
+            this.AnhVietTab.Controls.Add(this.button1);
             this.AnhVietTab.Controls.Add(this.comboBox1);
             this.AnhVietTab.Location = new System.Drawing.Point(4, 22);
-            this.AnhVietTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.AnhVietTab.Name = "AnhVietTab";
-            this.AnhVietTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.AnhVietTab.Size = new System.Drawing.Size(1186, 666);
+            this.AnhVietTab.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.AnhVietTab.Size = new System.Drawing.Size(788, 424);
             this.AnhVietTab.TabIndex = 0;
             this.AnhVietTab.Text = "Anh -  Việt";
             this.AnhVietTab.UseVisualStyleBackColor = true;
@@ -74,19 +74,17 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(376, 85);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Location = new System.Drawing.Point(216, 55);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(440, 28);
+            this.comboBox1.Size = new System.Drawing.Size(295, 21);
             this.comboBox1.TabIndex = 0;
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tabPage2.Size = new System.Drawing.Size(1186, 666);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(788, 424);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Việt - Anh";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -94,10 +92,9 @@
             // TraDoanTab
             // 
             this.TraDoanTab.Location = new System.Drawing.Point(4, 22);
-            this.TraDoanTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TraDoanTab.Name = "TraDoanTab";
-            this.TraDoanTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.TraDoanTab.Size = new System.Drawing.Size(1186, 666);
+            this.TraDoanTab.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.TraDoanTab.Size = new System.Drawing.Size(788, 424);
             this.TraDoanTab.TabIndex = 2;
             this.TraDoanTab.Text = "Tra đoạn";
             this.TraDoanTab.UseVisualStyleBackColor = true;
@@ -106,49 +103,54 @@
             // 
             this.TuDaTra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.TuDaTra.Location = new System.Drawing.Point(4, 22);
-            this.TuDaTra.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TuDaTra.Name = "TuDaTra";
-            this.TuDaTra.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.TuDaTra.Size = new System.Drawing.Size(1186, 666);
+            this.TuDaTra.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.TuDaTra.Size = new System.Drawing.Size(788, 424);
             this.TuDaTra.TabIndex = 3;
             this.TuDaTra.Text = "Từ đã tra";
             this.TuDaTra.UseVisualStyleBackColor = true;
-            // 
-            // ThemTuMoiTab
-            // 
-            this.ThemTuMoiTab.AllowDrop = true;
-            this.ThemTuMoiTab.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ThemTuMoiTab.Location = new System.Drawing.Point(4, 22);
-            this.ThemTuMoiTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.ThemTuMoiTab.Name = "ThemTuMoiTab";
-            this.ThemTuMoiTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.ThemTuMoiTab.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.ThemTuMoiTab.Size = new System.Drawing.Size(1186, 666);
-            this.ThemTuMoiTab.TabIndex = 5;
-            this.ThemTuMoiTab.Text = "Thêm từ mới";
-            this.ThemTuMoiTab.UseVisualStyleBackColor = true;
             // 
             // TuBatQuyTacTab
             // 
             this.TuBatQuyTacTab.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.hinh_nen_dep_cho_laptop_1366x768_1;
             this.TuBatQuyTacTab.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.TuBatQuyTacTab.Location = new System.Drawing.Point(4, 22);
-            this.TuBatQuyTacTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TuBatQuyTacTab.Name = "TuBatQuyTacTab";
-            this.TuBatQuyTacTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.TuBatQuyTacTab.Size = new System.Drawing.Size(1186, 666);
+            this.TuBatQuyTacTab.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.TuBatQuyTacTab.Size = new System.Drawing.Size(788, 424);
             this.TuBatQuyTacTab.TabIndex = 4;
             this.TuBatQuyTacTab.Text = "Từ bất quy tắc";
             this.TuBatQuyTacTab.UseVisualStyleBackColor = true;
             // 
+            // ThemTuMoiTab
+            // 
+            this.ThemTuMoiTab.AllowDrop = true;
+            this.ThemTuMoiTab.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ThemTuMoiTab.Location = new System.Drawing.Point(4, 22);
+            this.ThemTuMoiTab.Name = "ThemTuMoiTab";
+            this.ThemTuMoiTab.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.ThemTuMoiTab.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ThemTuMoiTab.Size = new System.Drawing.Size(788, 424);
+            this.ThemTuMoiTab.TabIndex = 5;
+            this.ThemTuMoiTab.Text = "Thêm từ mới";
+            this.ThemTuMoiTab.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1194, 692);
+            this.ClientSize = new System.Drawing.Size(796, 450);
             this.Controls.Add(this.VietAnhTab);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "mainForm";
             this.Text = "StudyE";
             this.Load += new System.EventHandler(this.mainForm_Load);
@@ -168,5 +170,6 @@
         private System.Windows.Forms.TabPage TuDaTra;
         private System.Windows.Forms.TabPage TuBatQuyTacTab;
         private System.Windows.Forms.TabPage ThemTuMoiTab;
+        private System.Windows.Forms.Button button1;
     }
 }
