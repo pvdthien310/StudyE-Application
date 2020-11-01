0d5c98caf0947252d2d8300ed9c54e2351355d75
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2020-11-01 16:14:40 +0700

    update

---
+++ WindowsFormsApp4/supportForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class supportForm : Form
+    {
+        mainForm parent;
+        public supportForm()
+        {
+            InitializeComponent();
+        }
+        public supportForm(mainForm parent, string word)
+        {
+            InitializeComponent();
+            textBox_support1.Text = word;
+            this.parent = parent;
+            DataManager.Instance.ConnectDatabseEV(word,ref textBox_support);
+            
+        }
+    }
+}
