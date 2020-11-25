2333fcd4513a44394c04b53d9bfc9b0f07c5b6d5
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2020-11-25 14:04:55 +0700

    A

--- WindowsFormsApp4/Picture_Dictionary.Designer.cs
+++ WindowsFormsApp4/Picture_Dictionary.Designer.cs
@@ -637,6 +637,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(95, 42);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(871, 559);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
--- WindowsFormsApp4/Picture_Dictionary.cs
+++ WindowsFormsApp4/Picture_Dictionary.cs
@@ -33,42 +33,114 @@ namespace WindowsFormsApp4
             if (sender == b1)
             {
                 group = "Animal";
-                DirectoryInfo dGroup = new DirectoryInfo("./PictureImage/Animal");
-                nameImage = dGroup.GetFiles();
-                panel1.Visible = true;
+                
             }
             else if (sender == b2)
             {
-
+                group = "Nature";
             }
-        }
-        private void ConnectDatabsePicture()
-        {
-            mycnt.Open(); // mo ket noi 
-            String path;
-            string search = "select * from PICTURE where GROUPS = '" + group + "' and STT = " + currentPicture + "";
-            SqlCommand com = new SqlCommand(search, mycnt); // truy van cau lenh vao sql
-            SqlDataAdapter ada = new SqlDataAdapter(com); // chuyen data tu sql ve trong ada
-            DataTable dt = new DataTable();
-            ada.Fill(dt); // do data tu ada va dt
-            if (dt.Rows.Count > 0)
+            else if (sender == b3)
             {
-                path = "./" + group + "/" + dt.Rows[0][""].ToString().Trim() + ".png";   //"./group/name.png"
-                
+                group = "Stationery";
             }
-            else
+            else if (sender == b4)
             {
-
+                group = "Music";
             }
-
-            //textBox1.Clear(); // Xóa từ cũ tránh trường hợp bị đùn gây bug
-
-            mycnt.Close(); // đóng kết nối
+            else if (sender == b5)
+            {
+                group = "Transport";
+            }
+            else if (sender == b6)
+            {
+                group = "Hospital";
+            }
+            else if (sender == b7)
+            {
+                group = "Buildings";
+            }
+            else if (sender == b8)
+            {
+                group = "Places";
+            }
+            else if (sender == b9)
+            {
+                group = "Subjects";
+            }
+            else if (sender == b10)
+            {
+                group = "Numbers";
+            }
+            else if (sender == b11)
+            {
+                group = "Counties";
+            }
+            else if (sender == b12)
+            {
+                group = "Home";
+            }
+            else if (sender == b13)
+            {
+                group = "Garden";
+            }
+            else if (sender == b14)
+            {
+                group = "Things";
+            }
+            else if (sender == b15)
+            {
+                group = "Colors";
+            }
+            else if (sender == b16)
+            {
+                group = "Letters";
+            }
+            else if (sender == b17)
+            {
+                group = "Kitchen";
+            }
+            else if (sender == b18)
+            {
+                group = "Body";
+            }
+            else if (sender == b19)
+            {
+                group = "Clothes";
+            }
+            else if (sender == b20)
+            {
+                group = "Food";
+            }
+            else if (sender == b21)
+            {
+                group = "Personal";
+            }
+            else if (sender == b22)
+            {
+                group = "Jobs";
+            }
+            else if (sender == b23)
+            {
+                group = "Bathroom";
+            }
+            else if (sender == b24)
+            {
+                group = "Sports";
+            }
+            else if (sender == b25)
+            {
+                group = "Computer";
+            }
+            DirectoryInfo dGroup = new DirectoryInfo("./PictureImage/" + group);
+            nameImage = dGroup.GetFiles();
+            panel1.Visible = true;
+            pictureBox1.Image = Image.FromFile(nameImage[0].FullName);
         }
-        
+       
         private void Pause_Click(object sender, EventArgs e)
         {
-
+            panel1.Visible = false;
+            
         }
 
         private void Right_Click(object sender, EventArgs e)
@@ -82,7 +154,6 @@ namespace WindowsFormsApp4
                 currentPicture = 0;
             }
             pictureBox1.Image = Image.FromFile(nameImage[currentPicture].FullName);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
        
