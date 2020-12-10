8816592775cce09aade4ce32780cc0941a9d901c
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2020-12-11 00:29:43 +0700

    a

--- encode/Form1.cs
+++ encode/Form1.cs
@@ -161,23 +161,23 @@ namespace encode
         private void button4_Click(object sender, EventArgs e)
         {
             cnn.Open();
-            string sql = "select * from PICTURE_SOURSE ";  // lay het du lieu trong bang sinh vien
+            string sql = "select * from PICTURE_SOURSE where NAME = 'cat' ";  // lay het du lieu trong bang sinh vien
             SqlCommand com = new SqlCommand(sql, cnn); //bat dau truy van
             com.CommandType = CommandType.Text;
             SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
             DataTable dt = new DataTable(); //tạo một kho ảo để lưu trữ dữ liệu
             da.Fill(dt);  // đổ dữ liệu vào kho
-            //cnn.Close();  // đóng kết nối
-            //dataGridView1.DataSource = dt; //đổ dữ liệu vào datagridview
+            cnn.Close();  // đóng kết nối
+            dataGridView1.DataSource = dt; //đổ dữ liệu vào datagridview
 
-            MemoryStream ms = new MemoryStream((byte[])dt.Rows[0]["ENCODE"]);
+            /*MemoryStream ms = new MemoryStream((byte[])dt.Rows[0]["ENCODE"]);
             Image a = Image.FromStream(ms);
 
             //Image a = byteArrayToImage((byte[])dt.Rows[0]["ENCODE"]);
             pictureBox1.BackgroundImage = a;
 
             cnn.Close();
-            cnn.Dispose();
+            cnn.Dispose();*/
         }
         
         
