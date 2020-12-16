87b9c6d22b2b0e67d9029d72dead8cff35b7026d
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2020-12-16 19:48:29 +0700

    a

--- WindowsFormsApp4/Program.cs
+++ WindowsFormsApp4/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Start());
+            Application.Run(new RoomChose());
         }
     }
 }
