99ea982da1ac514c7e5ceea737aae02795e1709b
TriThuc2321 <71278156+TriThuc2321@users.noreply.github.com>
2020-12-19 01:05:58 +0700

    a

--- encode/Form1.Designer.cs
+++ encode/Form1.Designer.cs
@@ -41,9 +41,9 @@ namespace encode
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(101, 30);
+            this.button1.Location = new System.Drawing.Point(59, 30);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 59);
+            this.button1.Size = new System.Drawing.Size(142, 59);
             this.button1.TabIndex = 0;
             this.button1.Text = "LoadSourse";
             this.button1.UseVisualStyleBackColor = true;
